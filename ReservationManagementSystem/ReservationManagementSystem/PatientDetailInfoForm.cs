bfce9c53c6caee5a7f7d4c798ce5c430018987df
honghanh2608 <nguyenthihonghanh2608@gmail.com>
2021-12-20 09:38:28 +0700

    create form patient list

---
+++ ReservationManagementSystem/ReservationManagementSystem/PatientDetailInfoForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReservationManagementSystem
+{
+    public partial class PatientDetailInfoForm : Form
+    {
+        public string PatientId { get; set; }
+        public PatientDetailInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PatientDetailInfoForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = PatientId;
+        }
+    }
+}
